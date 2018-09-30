4b46deff46600ac98832e91cf8358e35e92fac59
Jefferson Nascimento <jefferson.nascimento3@fatec.sp.gov.br>
2018-09-29 21:41:36 -0300

    Add repository to RiverController

--- src/main/java/com/boot/rivers/controllers/RiverController.java
+++ src/main/java/com/boot/rivers/controllers/RiverController.java
@@ -1,5 +1,6 @@
 package com.boot.rivers.controllers;
 
+import com.boot.rivers.repositories.SampleRepository;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,19 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("api/v1/rivers")
+@RequestMapping("api/v1/rivers")    
 public class RiverController {
 	@Autowired
 	private RestTemplate restTemplate;
+        
+        @Autowired
+        private SampleRepository sampleRepository;
 
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<String> list() {
+            
+            System.out.println(sampleRepository.findAll());
+            
 		HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
-        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
-        String url = "http://riverbrain.com/api/v1/rivers";
-        ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);        
+                headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+                headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+                HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+                String url = "http://riverbrain.com/api/v1/rivers";
+                ResponseEntity<String> res = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);        
 		return res;
 	}
 
